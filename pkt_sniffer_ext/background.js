c4fc04a3c06d8a4ebde393e835b00256fdbf6a53
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-11-21 13:24:37 +0530

    db connection configurations

--- pkt_sniffer_ext/background.js
+++ pkt_sniffer_ext/background.js
@@ -1,65 +1,58 @@
+// const { response } = require("express");
+
 (function () {
+  var url_filter_obj = { urls: ["*://*/*"] };
+  var post_obj = null;
 
+  async function saveUser(data) {
+    let response = await fetch(url, { method: "POST", body: data })
+      .then((response) => response.json())
+      .then()
+      .catch();
+  }
 
-    var url_filter_obj = { 'urls': ['*://*/*'] };
-    var post_obj = null;
+  function capture(req_details) {
+    try {
+      var data = {
+        initiator: req_details.initiator,
+        ip: req_details.ip,
+        method: req_details.method,
+        url: req_details.url,
+        time: new Date(req_details.timeStamp).toLocaleString(),
+      };
+      console.log(data);
+    } catch (e) {}
+  }
 
-    async function saveUser(data) {
-      let response = await fetch(url, { method: "POST", body: data })
-        .then()
-        .then()
-        .catch();
-      let callResponse = await response.json();
+  chrome.extension.onConnect.addListener(function (port) {
+    try {
+      console.log(port);
+    } catch (e) {
+      console.log();
     }
 
-    function capture(req_details) {
-      try {
-        var data = {
-          initiator: req_details.initiator,
-          ip: req_details.ip,
-          method: req_details.method,
-          url: req_details.url,
-          time: new Date(req_details.timeStamp).toLocaleString(),
-        };
-        console.log(data);
-      } catch (e) {}
-    }
+    webRequestListener(true);
 
-    chrome.extension.onConnect.addListener(function (port) {
-
-        try {
-            console.log(port)
-        } catch (e) {
-            console.log()
-        }
-
-        webRequestListener(true);
-
-        post_obj.onDisconnect.addListener(function () {
-            webRequestListener(false);
-        })
-
+    post_obj.onDisconnect.addListener(function () {
+      webRequestListener(false);
     });
+  });
 
+  function webRequestListener(listen) {
+    chrome.webRequest.onBeforeSendHeaders.removeListener(capture);
+    chrome.webRequest.onHeadersReceived.removeListener(capture);
+    chrome.webRequest.onCompleted.removeListener(capture);
 
-    function webRequestListener(listen) {
-
-        chrome.webRequest.onBeforeSendHeaders.removeListener(capture);
-        chrome.webRequest.onHeadersReceived.removeListener(capture);
-        chrome.webRequest.onCompleted.removeListener(capture);
-
-        if (listen) {
-            chrome.webRequest.onBeforeSendHeaders.addListener(capture, url_filter_obj, ["requestHeaders"]);
-            chrome.webRequest.onHeadersReceived.addListener(capture, url_filter_obj, ["responseHeaders"]);
-            chrome.webRequest.onCompleted.addListener(capture, url_filter_obj);
-        }
+    if (listen) {
+      chrome.webRequest.onBeforeSendHeaders.addListener(
+        capture,
+        url_filter_obj,
+        ["requestHeaders"]
+      );
+      chrome.webRequest.onHeadersReceived.addListener(capture, url_filter_obj, [
+        "responseHeaders",
+      ]);
+      chrome.webRequest.onCompleted.addListener(capture, url_filter_obj);
     }
-
+  }
 })();
-
-
-
-
-
-
-
